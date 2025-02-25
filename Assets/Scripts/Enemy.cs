76db5bdd701222728e5e92cca0ef7b86adf4706e
sbagos27 <sbagos@csumb.edu>
2025-02-25 13:34:39 -0800

    imported assets

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+      Debug.Log("Ouch!");
+    }
+}
